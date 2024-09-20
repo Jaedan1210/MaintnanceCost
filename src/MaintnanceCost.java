df19fbb2411d253d929e32dc53039f8ec734b461
Jaedan Saunders <saundej7@mail.uc.edu>
2024-09-20 12:17:57 -0400

    Maintnance Cost

---
+++ src/MaintnanceCost.java
@@ -0,0 +1,17 @@
+public class MaintnanceCost {
+    public static void main(String[] args) {
+        System.out.println("Input Personal costs for each season below!");
+//
+
+        double springCost = 1;
+        double summerCost = 2;
+        double fallCost = 3;
+        double winterCost = 4;
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Your personal cost for Spring was " + springCost);
+        System.out.println("Your personal cost for Summer was " + summerCost);
+        System.out.println("Your personal cost for Fall was " + fallCost);
+        System.out.println("Your personal cost for Winter was " + winterCost);
+        System.out.println("Your personal total cost for the year was " + totalCost);
+} }
